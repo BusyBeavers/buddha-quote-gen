d00431c832171db8e2145cdddd01843699dc6ec1
Ford <codingasford@gmail.com>
2026-03-12 17:27:57 -0700

    updated package.json with start script command and added more flexible port to index.js for web host service compatibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-const PORT = 5387;
+const PORT = process.env.PORT || 5387;
 
 const app = express();
 
@@ -9,24 +9,24 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server listening on port: ${PORT}`);
 });
 
-app.post('/', async (req, res) => {
-    try {
-        const response = await fetch('https://buddha-api.com/api/random');
-        const data = await response.json();
+app.post("/", async (req, res) => {
+  try {
+    const response = await fetch("https://buddha-api.com/api/random");
+    const data = await response.json();
 
-        if (!response.ok) {
-            return res.status(response.status).json({
-                error: 'error calling api',
-                response: data
-            });
-        }
-
-        return res.status(200).json(data);
-    } catch (err) {
-        console.error("error calling api");
-        return res.status(500).json({ error: "error calling api" });
+    if (!response.ok) {
+      return res.status(response.status).json({
+        error: "error calling api",
+        response: data,
+      });
     }
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.error("error calling api");
+    return res.status(500).json({ error: "error calling api" });
+  }
 });
